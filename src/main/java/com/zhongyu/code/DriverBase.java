69af93e0d76484a0dbb3693a2675f4f4b38ab0ea
Aimee <zhongyu54321@gmail.com>
2018-04-10 20:02:58 +0800

    封装BaseDriver

---
+++ src/main/java/com/zhongyu/code/DriverBase.java
@@ -0,0 +1,13 @@
+package com.zhongyu.code;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by YuChan on 02/26/2017.
+ */
+public class DriverBase {
+
+    private Logger logger = LoggerFactory.getLogger(DriverBase.class);
+
+}
