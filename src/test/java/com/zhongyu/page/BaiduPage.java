1b5f061d06307a57691eff38eeda802b27febd62
Aimee <zhongyu54321@gmail.com>
2018-04-10 20:18:15 +0800

    添加百度翻译PageElement

---
+++ src/test/java/com/zhongyu/page/BaiduPage.java
@@ -0,0 +1,28 @@
+package com.zhongyu.page;
+
+import com.zhongyu.code.BasePage;
+import com.zhongyu.code.DriverBase;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by YuChan on 02/26/2017.
+ */
+public class BaiduPage extends BasePage {
+
+    public BaiduPage(DriverBase driver) {
+        super(driver);
+    }
+
+    public WebElement getInputElement() {
+        return element(getLocator("fanyi", "row_input"));
+    }
+
+    public WebElement getIranslateElement() {
+        return element(getLocator("fanyi", "translate"));
+    }
+
+    public WebElement getResultEmelent() {
+        return element(getLocator("fanyi", "output"));
+    }
+
+}
